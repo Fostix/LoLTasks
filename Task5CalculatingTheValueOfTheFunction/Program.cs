476fcb38f2be337fdcd0d33b144b5d7b70cff205
Fostix <accretiondiskthat@gmail.com>
2022-01-06 19:09:54 +0500

    task 5 calculating the value of the function

--- Task5CalculatingTheValueOfTheFunction/Program.cs
+++ Task5CalculatingTheValueOfTheFunction/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу вычисления значения функции y = f(a)
+// f(x) = x^2+1
+
+Console.Write("Enter number: ");
+int number = int.Parse(Console.ReadLine());
+number = number * number + 1;
+Console.WriteLine(number);
