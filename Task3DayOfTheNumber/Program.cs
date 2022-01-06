7101322cbc618a038ab5351419cf4a3689d6004d
Fostix <accretiondiskthat@gmail.com>
2022-01-06 18:25:29 +0500

    make task 3 day of week

--- Task3DayOfTheNumber/Program.cs
+++ Task3DayOfTheNumber/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// По заданному номеру дня недели вывести его название.
+
+Console.Write("Enter number day: ");
+int number = int.Parse(Console.ReadLine());
+
+string[] array = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+number = number - 1;
+
+if (number >= 0 & number < 31)
+{
+    while (number > 6)
+    {
+        number = number - 7;
+    }
+    if (number < 6)
+    {
+        Console.Write(array[number]);
+    }
+}
+else
+{
+    Console.WriteLine("Incorrect number day");
+}
