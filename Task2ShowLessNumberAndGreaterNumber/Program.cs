ebd62a7de94356996c8c7ccbfd5ae97874910759
Fostix <accretiondiskthat@gmail.com>
2022-01-06 10:19:51 +0500

    task 2 show less number and greater number corrected the mistake

--- Task2ShowLessNumberAndGreaterNumber/Program.cs
+++ Task2ShowLessNumberAndGreaterNumber/Program.cs
@@ -1,15 +1,10 @@
 ﻿// Даны два числа. Показать большее и меньшее число.
 
 Console.Write("Enter first number: ");
-int firstNumber = int.Parse(Console.ReadLine());
+int firstN = int.Parse(Console.ReadLine());
 Console.Write("Enter second number: ");
-int secondNumber = int.Parse(Console.ReadLine());
+int secondN = int.Parse(Console.ReadLine());
 
-if (firstNumber > secondNumber)
-{
-    Console.WriteLine("First number: " + firstNumber + " greater than second number: " + secondNumber + ".");
-}
-else
-{
-    Console.WriteLine("First number: " + firstNumber + " less than second number: "  +secondNumber + ".");
-}
+if (firstN < secondN) Console.WriteLine($"first number: {firstN} less than second number: {secondN}.");
+if (firstN > secondN) Console.WriteLine($"first number: {firstN} more than second number: {secondN}.");
+if (firstN == secondN) Console.Write($"numbers equals: {firstN}, {secondN}.");
