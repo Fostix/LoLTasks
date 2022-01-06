213ef15065bc10412d0dc78c1cbcf7ef4c3977fc
Fostix <accretiondiskthat@gmail.com>
2022-01-06 20:28:37 +0500

    task 7 show number from -N to N

---
+++ Task7ShowNumberFrom-NToN/Program.cs
@@ -0,0 +1,22 @@
+﻿// Показать числа от -N до N.
+
+Console.Write("Enter negative number: ");
+int number = int.Parse(Console.ReadLine());
+
+int tempNumber;
+if (number > 0)
+{
+    tempNumber = +number;
+    number = -number;
+    Console.Write(number + " ");
+}
+else
+{
+    tempNumber = -number;
+    Console.Write(number + " ");
+}
+
+while (number != tempNumber)
+{
+    Console.Write($"{number = number + 1} ");
+}
