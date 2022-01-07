c82f0663799c23d9293419e5283b8868ccadaf8d
Fostix <accretiondiskthat@gmail.com>
2022-01-07 09:44:12 +0500

    make task 8 show evenn number from 2 to N

---
+++ Task8ShowEvenNumberFrom1ToN/Program.cs
@@ -0,0 +1,26 @@
+﻿// Показать четные числа от 1 до N.
+
+Console.Write("Enter number: ");
+int number = int.Parse(Console.ReadLine()),
+N = 2;
+
+if (number < 1)
+{
+    number = -number;
+}
+
+if (0 != number % 2)
+{
+    number--;
+}
+
+while (N < number)
+{
+    Console.Write(N + " ");
+    N += 2;
+}
+if (number != 0)
+{
+
+    Console.Write(number);
+}
