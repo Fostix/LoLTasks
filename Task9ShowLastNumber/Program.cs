b504af2aa11652d9bde5050ef555330fb84feb3c
Fostix <accretiondiskthat@gmail.com>
2022-01-07 10:09:32 +0500

    task 9 show last number

---
+++ Task9ShowLastNumber/Program.cs
@@ -0,0 +1,7 @@
+﻿// Показать последнюю цифру трёхзначного числа.
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine("Total number: " + number);
+
+number = number % 10;
+Console.Write("Last number: " + number);
