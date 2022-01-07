7029b0860f5fad441cf105df7936a860a7f0bf8d
Fostix <accretiondiskthat@gmail.com>
2022-01-07 21:23:22 +0500

    maked task 12 remove the second digit of a number

--- Task12RemoveTheSecondDigitOfANumber/Program.cs
+++ Task12RemoveTheSecondDigitOfANumber/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Удалить вторую цифру трёхзначного числа.
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine(number);
+int lastDigit = number % 10;
+number = number / 100;
+int firstDigit = number % 10;
+firstDigit = firstDigit * 10;
+firstDigit = firstDigit + lastDigit;
+Console.WriteLine(firstDigit);
