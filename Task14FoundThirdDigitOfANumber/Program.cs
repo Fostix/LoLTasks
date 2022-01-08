7cb46d7b3961d41ebcf083d642e46ea4981c2750
Fostix <accretiondiskthat@gmail.com>
2022-01-08 18:08:50 +0500

    maked task 14 found third digit of a number

--- Task14FoundThirdDigitOfANumber/Program.cs
+++ Task14FoundThirdDigitOfANumber/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Найти третью цифру числа или сообщить, что её нет.
+
+Console.Write("Enter number: ");
+int number = int.Parse(Console.ReadLine());
+if (number < 0)
+{
+    number = number * (-1);
+}
+while (number > 1000)
+{
+    number = number / 10;
+}
+if (number > 99)
+{
+    Console.WriteLine(number % 10);
+}
+else
+{
+    Console.WriteLine("no third digit");
+}
