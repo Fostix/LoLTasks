172cebf984e6a2b18cdef51f79b1828656b6d068
Fostix <accretiondiskthat@gmail.com>
2022-01-09 15:31:33 +0500

    maked task 17 check if one is a square of a other

--- Task17CheckIfOneIsTheSquareOfTheOther/Program.cs
+++ Task17CheckIfOneIsTheSquareOfTheOther/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// По двум заданным числам проверять является ли одно квадратом другого.
+
+Console.Write("Enter first number: ");
+int firstNumber = int.Parse(Console.ReadLine());
+Console.Write("Enter second number: ");
+int secondNumber = int.Parse(Console.ReadLine());
+
+if (secondNumber == firstNumber * firstNumber)
+{
+    Console.Write("The second number: " + secondNumber + " is the square of the first: " + firstNumber + ".");
+}
+if (firstNumber == secondNumber * secondNumber)
+{
+    Console.Write("The first number" + firstNumber + " is the square of the second: " + secondNumber + ".");
+}
