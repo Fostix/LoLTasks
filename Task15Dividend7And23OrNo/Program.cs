de3b7e7f536dab91483fd7aa70e877ed37e7c742
Fostix <accretiondiskthat@gmail.com>
2022-01-09 08:53:16 +0500

    maked task 15 dividend 7 and 23 or not

--- Task15Dividend7And23OrNo/Program.cs
+++ Task15Dividend7And23OrNo/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Дано число. Проверить кратно ли оно 7 и 23.
+
+Console.Write("Enter number: ");
+int number = int.Parse(Console.ReadLine()),
+mod7 = number % 7,
+mod23 = number % 23;
+if (0 == mod23)
+{
+    Console.WriteLine("multiple of 23. equals: " + number / 23);
+}
+else
+{
+    Console.WriteLine("no multiple of 23.");
+    
+}
+if (0 == mod7)
+{
+    Console.WriteLine("multiple of 7. equals: " + number / 7);
+}
+else
+{
+    Console.WriteLine("no multiple of 7.");
+}
