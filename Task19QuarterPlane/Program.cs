ee1f28a164e5ed2cc59d523e41ed6c2de33fdb85
Fostix <accretiondiskthat@gmail.com>
2022-01-10 19:27:15 +0500

    maked task 19 quarter plane

--- Task19QuarterPlane/Program.cs
+++ Task19QuarterPlane/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Определить номер четверти плоскости, в которой находится точка с координатами Х и У, причем X ≠ 0 и Y ≠ 0.
+
+Console.Write("Enter X-axis: ");
+int xAxis = int.Parse(Console.ReadLine());
+Console.Write("Enter Y-axis: ");
+int yAxis = int.Parse(Console.ReadLine());
+
+if (xAxis > 0 && yAxis > 0)
+{
+    Console.WriteLine("First quarter of the coordinate plane");
+}
+if (xAxis < 0 && yAxis > 0)
+{
+    Console.WriteLine("Second quarter of the coordinate plane");
+}
+if (xAxis < 0 && yAxis < 0)
+{
+    Console.WriteLine("Third quarter of the coordinate plane");
+}
+if (xAxis > 0 && yAxis < 0)
+{
+    Console.WriteLine("Fourth quarter of the coordinate plane");
+}
