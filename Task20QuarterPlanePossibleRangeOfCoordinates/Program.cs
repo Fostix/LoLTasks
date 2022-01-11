59292625c4c5253db4587c175ef2694f6fb9afd3
Fostix <accretiondiskthat@gmail.com>
2022-01-11 20:55:44 +0500

    maked task 20 quarter plane possible range of coordinates

---
+++ Task20QuarterPlanePossibleRangeOfCoordinates/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат.
+
+Console.Clear();
+Console.Write("Enter number: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number == 1)
+{
+    Console.WriteLine("X from 0 to infinity; Y from 0 to infinity");
+}
+if (number == 2)
+{
+    Console.WriteLine("X from 0 to - infinity; Y from 0 to infinity");
+}
+if (number == 3)
+{
+    Console.WriteLine("X from 0 to -infinity; Y from 0 to -infinity");
+}
+if (number == 4)
+{
+    Console.WriteLine("X from 0 to infinity; Y from 0 to -infinity");
+}
+else
+{
+    Console.WriteLine("there is no such: " + number + " plane of a quarter");
+}
