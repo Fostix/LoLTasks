abb014bd2f7308f9435896ec2a100b7e5ebdfeb1
Fostix <accretiondiskthat@gmail.com>
2022-01-17 21:57:04 +0500

    maked task 24 find cubes of numbers

---
+++ Task24FindCubesOfNumbers/Program.cs
@@ -0,0 +1,8 @@
+﻿// Найти кубы чисел от 1 до N.
+
+Console.Write("Enter number: ");
+ulong finishNumber = ulong.Parse(Console.ReadLine());
+for (ulong i = 1; i <= finishNumber; i++)
+{
+    Console.WriteLine(i + "^3" + " = " + i * i * i);
+}
