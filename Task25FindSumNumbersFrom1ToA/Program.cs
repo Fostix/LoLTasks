7a8d7499e7e4ce54c4da278f73a153cdf8156a28
Fostix <accretiondiskthat@gmail.com>
2022-01-18 18:47:40 +0500

    maked task 25 find sum numbers from 1 to a

--- Task25FindSumNumbersFrom1ToA/Program.cs
+++ Task25FindSumNumbersFrom1ToA/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Найти сумму чисел от 1 до А.
+
+Console.Write("Enter number: ");
+int number = int.Parse(Console.ReadLine());
+
+for (int i = number - 1; i >= 1; i--)
+{
+    number += i;
+}
+
+Console.WriteLine(number);
