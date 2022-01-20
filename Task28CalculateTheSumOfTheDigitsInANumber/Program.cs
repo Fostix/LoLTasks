bbfa2705fdead466694c602636a416a687372363
Fostix <accretiondiskthat@gmail.com>
2022-01-20 21:56:01 +0500

    maked task 28 calculate the sum of the digits in a number

---
+++ Task28CalculateTheSumOfTheDigitsInANumber/Program.cs
@@ -0,0 +1,20 @@
+﻿// Подсчитать сумму цифр в числе.
+
+Console.Write("Enter number: ");
+int number = int.Parse(Console.ReadLine()),
+sum = 0,
+digit;
+
+if (number < 0)
+{
+    number = number * -1;
+}
+
+for (int i = 0; number > 0; i++)
+{
+    digit = number % 10;
+    sum += digit;
+    number /= 10;
+}
+
+    Console.WriteLine("The sum of the digits in a number: " + sum);
