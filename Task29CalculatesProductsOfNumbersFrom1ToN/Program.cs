4fd10658d69a8c1ed095de89b8aa338d73c97a00
Fostix <accretiondiskthat@gmail.com>
2022-01-28 07:23:15 +0500

    maked task 29 calculates products of numbers from 1 to N

---
+++ Task29CalculatesProductsOfNumbersFrom1ToN/Program.cs
@@ -0,0 +1,11 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N.
+
+Console.Write("Enter first factor: ");
+int firstFactor = int.Parse(Console.ReadLine());
+Console.Write("Enter number: ");
+int finish = int.Parse(Console.ReadLine());
+
+for (int i = 1; i < finish; i++)
+{
+    Console.WriteLine(firstFactor + " * " + i + " = " + firstFactor * i);
+}
