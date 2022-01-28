b1791c32e23642717ee7dd718aacd7b1f0d52d33
Fostix <accretiondiskthat@gmail.com>
2022-01-28 07:43:40 +0500

    task 30 cubes of numbers ending in an even number

---
+++ Task30CubesOfNumbersEndingInAnEvenNumber/Program.cs
@@ -0,0 +1,16 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру.
+
+Console.Write("Enter number: ");
+int finishNumber = int.Parse(Console.ReadLine()),
+result,
+cube = 3;
+
+for (int i = 0; i < finishNumber; i++)
+{
+    result = i * i * i;
+    if(result % 2 == 0)
+    {
+        Console.WriteLine( i + "^" + cube + " = " + result);
+    }
+}
+
