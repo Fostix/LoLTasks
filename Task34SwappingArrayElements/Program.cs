410823cf530be293842f81007821ab1590d87f4b
Fostix <accretiondiskthat@gmail.com>
2022-02-03 07:15:00 +0500

    maked task 34 swapping array elements

---
+++ Task34SwappingArrayElements/Program.cs
@@ -0,0 +1,38 @@
+﻿// Написать программу замену элементов массива на противоположные.
+Console.Write("Enter array length: ");
+int arrayLength = int.Parse(Console.ReadLine());
+int[] array = new int[arrayLength];
+
+ArrayRandomFillMethod(array);
+PrintArray(array);
+Console.WriteLine();
+SwapArrayMethod(array);
+PrintArray(array);
+
+
+void ArrayRandomFillMethod(int[] fill)
+{
+    for (int i = 0; i < fill.Length; i++)
+    {
+        fill[i] = new Random().Next(-9, 10);
+    }
+}
+
+
+void SwapArrayMethod(int[] swap)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = -array[i];
+    }
+}
+
+
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
